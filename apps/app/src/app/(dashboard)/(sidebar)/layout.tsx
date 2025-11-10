770a7905a2069995a870eaa445219bcab074d725
Raf Mevis <raf.mevis@gmail.com>
2025-11-10 17:57:37 +0100

    Build fix.

--- apps/app/src/app/(dashboard)/(sidebar)/layout.tsx
+++ apps/app/src/app/(dashboard)/(sidebar)/layout.tsx
@@ -3,6 +3,8 @@ import { Sidebar } from "@/components/sidebar";
 import { HydrateClient, getQueryClient, trpc } from "@/trpc/server";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function Layout({
   children,
 }: {

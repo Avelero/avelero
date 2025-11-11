d7278db5d3d544d0e1ec723e4214a35dde497843
Raf Mevis <raf.mevis@gmail.com>
2025-11-11 15:33:49 +0100

    Fixed login v2.

--- packages/ui/src/components/input.tsx
+++ packages/ui/src/components/input.tsx
@@ -10,8 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex w-full border bg-background px-3 py-2 !type-p ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-tertiary focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 transition ease-in-out duration-200",
-          "[&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none [&[type=number]]:appearance-none",
+          "flex w-full rounded-none border border-border bg-background px-3 py-2 !type-p ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-tertiary focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 transition-colors [-webkit-appearance:none] [appearance:none]",
           className,
         )}
         ref={ref}

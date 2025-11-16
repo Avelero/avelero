67dedc9da9ad440b68beed884cf03c8ca3a0457f
tr4moryp <m.ouallaf007@gmail.com>
2025-11-16 14:26:33 -0500

    chore: remove apps/web redesign files not related to bulk import

--- apps/web/src/lib/ratelimit.ts
+++
@@ -1,9 +0,0 @@
-import "server-only";
-import { Ratelimit } from "@upstash/ratelimit";
-import { client } from "@v1/kv/client";
-
-// Contact form specific rate limiter - 3 submissions per hour
-export const contactFormRateLimit = new Ratelimit({
-  limiter: Ratelimit.slidingWindow(3, "1 h"),
-  redis: client,
-});

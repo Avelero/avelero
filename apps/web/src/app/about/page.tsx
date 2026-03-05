92bd6c0cfc822bead0b73800ea740f5a4445840e
Raf Mevis <raf.mevis@gmail.com>
2026-03-05 16:11:26 +0100

    Fixed mobile navigation.

--- apps/web/src/app/about/page.tsx
+++ apps/web/src/app/about/page.tsx
@@ -24,7 +24,7 @@ export default async function AboutPage() {
             <div className="flex flex-col gap-4">
                 <h5 className="text-h5 text-foreground/50">About</h5>
                 <h1 className="text-[10vw] sm:text-[clamp(3.8rem,9.5vw,4.8rem)] md:text-[clamp(4.8rem,10vw,6.7rem)] lg:text-[clamp(6.7rem,10.5vw,8.625rem)] xl:text-[8.625rem] leading-none text-foreground">
-                    <span className="text-primary">We care</span> {" "} 
+                    <span className="text-primary">We care</span>{" "}
                     about product passpo<span className="tracking-wide">rt</span>s
                 </h1>
             </div>
